d6e749ef8f3bdad52ca27973b3df2e6ca6ce3182
deemonic <michael.deeming90@gmail.com>
2025-03-17 20:40:06 +0000

    added contructor to strategies

--- src/Strategies/LibreOfficeStrategy.php
+++ src/Strategies/LibreOfficeStrategy.php
@@ -6,6 +6,30 @@ use Blaspsoft\Doxswap\Contracts\ConversionStrategy;
 
 class LibreOfficeStrategy implements ConversionStrategy
 {
+    /**
+     * The path to the LibreOffice binary.
+     *
+     * @var string
+     */
+    protected string $path;
+
+    /**
+     * Create a new LibreOfficeStrategy instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->path = config('doxswap.drivers.libreoffice.path');
+    }
+
+    /**
+     * Convert a file to a new format.
+     *
+     * @param string $inputFile
+     * @param string $outputFile
+     * @return string
+     */
     public function convert(string $inputFile, string $outputFile): string
     {
         $command = "soffice --headless --convert-to {$outputFile} {$inputFile}";
--- src/Strategies/PandocStrategy.php
+++ src/Strategies/PandocStrategy.php
@@ -6,9 +6,33 @@ use Blaspsoft\Doxswap\Contracts\ConversionStrategy;
 
 class PandocStrategy implements ConversionStrategy
 {
+    /**
+     * The path to the Pandoc binary.
+     *
+     * @var string
+     */
+    protected string $path;
+
+    /**
+     * Create a new PandocStrategy instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->path = config('doxswap.drivers.pandoc.path');
+    }
+
+    /**
+     * Convert a file to a new format.
+     *
+     * @param string $inputFile
+     * @param string $outputFile
+     * @return string
+     */
     public function convert(string $inputFile, string $outputFile): string
     {
-        $command = "pandoc {$inputFile} -o {$outputFile}";
+        $command = "{$this->path} {$inputFile} -o {$outputFile}";
         exec($command);
         return $outputFile;
     }
