9df49ea483a4da71b2231cefdb97b388342bd32b
deemonic <michael.deeming90@gmail.com>
2025-03-20 11:05:19 +0000

    odt and png tests added

---
+++ tests/Integration/PngConversionTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Blaspsoft\Doxswap\Tests\Integration;
+
+use Blaspsoft\Doxswap\Converter;
+use Blaspsoft\Doxswap\Tests\TestCase;
+use Illuminate\Support\Facades\Storage;
+
+class PngConversionTest extends TestCase
+{
+    protected Converter $converter;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        
+        Storage::fake('local');
+
+        $this->converter = new Converter();
+    }
+    
+    public function testPngToPdfConversion()
+    {
+        Storage::disk('local')->put('test.png', file_get_contents(__DIR__ . '/../Stubs/sample.png'));
+        $this->converter->convert('test.png', 'pdf');
+        $this->assertTrue(Storage::disk('local')->exists('test.pdf'));
+    }
+
+    public function testPngToJpgConversion()
+    {
+        Storage::disk('local')->put('test.png', file_get_contents(__DIR__ . '/../Stubs/sample.png'));
+        $this->converter->convert('test.png', 'jpg');
+        $this->assertTrue(Storage::disk('local')->exists('test.jpg'));
+    }
+
+    public function testPngToSvgConversion()
+    {
+        Storage::disk('local')->put('test.png', file_get_contents(__DIR__ . '/../Stubs/sample.png'));
+        $this->converter->convert('test.png', 'svg');
+        $this->assertTrue(Storage::disk('local')->exists('test.svg'));
+    }
+}
